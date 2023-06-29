963ee4e7bb23d9c0dc69523aba8aa69ded649332
olegvallas <oleg.vallas@gmail.com>
2023-06-29 09:10:38 +0200

    Change engine drive to async one

--- database/engine.py
+++ database/engine.py
@@ -3,7 +3,6 @@ DB engine and session management
 """
 import os
 
-import sqlalchemy
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.orm import sessionmaker
