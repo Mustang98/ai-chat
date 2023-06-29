f6b1c2469bbf171054c83a1516798d18e92a1f7f
olegvallas <oleg.vallas@gmail.com>
2023-06-29 16:17:13 +0200

    Front end design

--- frontend/app.js
+++ frontend/app.js
@@ -1,18 +1,10 @@
-function fetchCharacters() {
-  fetch('http://35.193.20.238:8000/characters')
-    .then(response => response.json())
-    .then(data => {
-      const characterList = document.getElementById('characterList');
-
-      data.characters.forEach(character => {
-        const listItem = document.createElement('li');
-        listItem.textContent = character.name;
-        characterList.appendChild(listItem);
+angular.module('characterApp', [])
+  .controller('characterController', function($scope, $http) {
+    $http.get('http://35.193.20.238:8000/characters')
+      .then(function(response) {
+        $scope.characters = response.data.characters;
+      })
+      .catch(function(error) {
+        console.error('Error:', error);
       });
-    })
-    .catch(error => {
-      console.error('Error:', error);
-    });
-}
-
-document.addEventListener('DOMContentLoaded', fetchCharacters);
+  });
