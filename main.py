d1ae077398227b2ac894d85d09a1cfe77d4aa5d6
olegvallas <oleg.vallas@gmail.com>
2023-06-29 15:22:31 +0200

    Added create message endpoint

--- main.py
+++ main.py
@@ -2,14 +2,13 @@ import os
 import logging
 
 from fastapi import FastAPI
-from routes import users
-from routes import characters
-from routes import dialogues
+from routes import users, characters, dialogues, messages
 
 logging.basicConfig(level=logging.DEBUG)
 
 app = FastAPI()
-app.include_router(user.router)
-app.include_router(character.router)
-app.include_router(dialogue.router)
+app.include_router(users.router)
+app.include_router(characters.router)
+app.include_router(dialogues.router)
+app.include_router(messages.router)
 
