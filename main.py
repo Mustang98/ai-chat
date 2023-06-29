fb44818f80f241aadced06857c83ec24f3d2a5b9
olegvallas <oleg.vallas@gmail.com>
2023-06-29 09:42:26 +0200

    Added get characters endpoint

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import logging
 
 from fastapi import FastAPI
 from routes import user
+from routes import character
 
 logging.basicConfig(level=logging.DEBUG)
 
 app = FastAPI()
 app.include_router(user.router)
+app.include_router(character.router)
