63ea4fb345f675e02a76dbdd3ee0bf2ff7cc0c1f
olegvallas <oleg.vallas@gmail.com>
2023-06-30 06:00:11 +0200

    Fix REST API tests

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -4,22 +4,21 @@ This file contains the tests for the REST API routes.
 from fastapi.testclient import TestClient
 from main import app
 
-client = TestClient(app)
 
+def test_get_characters():
+    """
+    Test GET /characters endpoint.
 
-# def test_get_characters():
-#     """
-#     Test GET /characters endpoint.
-#
-#     :return: None
-#     """
-#     response = client.get("/characters")
-#     assert response.status_code == 200
-#     data = response.json()
-#     assert isinstance(data, dict)
-#     assert "characters" in data
-#     assert isinstance(data["characters"], list)
-#
+    :return: None
+    """
+    client = TestClient(app)
+    response = client.get("/characters")
+    assert response.status_code == 200
+    data = response.json()
+    assert isinstance(data, dict)
+    assert "characters" in data
+    assert isinstance(data["characters"], list)
+
 
 def test_create_message():
     """
@@ -27,6 +26,7 @@ def test_create_message():
 
     :return: None
     """
+    client = TestClient(app)
     # Create a test message
     payload = {"dialogue_id": "1b40520b-05d6-419c-a136-1b5b72e9a3e5", "content": "Test message"}
     response = client.post("/messages", json=payload)
